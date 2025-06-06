2eb4a46f48a1796172979ed1f37ec4dd646ee352
AndreiRech <andreirech22@outlook.com>
2025-06-06 16:48:48 -0300

    Initial Commit

---
+++ Clairfy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clairfy
+//
+//  Created by Andrei Rech on 06/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
