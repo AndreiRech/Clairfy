a81ef74a7b7091183e99fae648fa26cf97f45a84
AndreiRech <andreirech22@outlook.com>
2025-06-11 14:07:44 -0300

    feature: add fonts

--- Clairfy/Fonts/Fonts.swift
+++ Clairfy/Fonts/Fonts.swift
@@ -1,3 +1,13 @@
+import UIKit
+
 struct Fonts {
-    // TODO: Add fonts
+    static let body = UIFont(name: "SFProText-Regular", size: 17)
+    
+    static let headline = UIFont(name: "SFProText-Semibold", size: 17)
+    static let subheadline = UIFont(name: "SFProText-Regular", size: 15)
+    
+    static let largeTitle = UIFont(name: "SFProText-Bold", size: 34)
+    static let title2 = UIFont(name: "SFProText-Bold", size: 24)
+    
+    static let timer = UIFont(name: "SFProText-Bold", size: 42)
 }
