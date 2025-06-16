50f5316d0da60beab8a5f4ec36bd74530ba4f19d
AndreiRech <andreirech22@outlook.com>
2025-06-16 15:01:43 -0300

    feature: new fonts

--- Clairfy/Fonts/Fonts.swift
+++ Clairfy/Fonts/Fonts.swift
@@ -2,12 +2,13 @@ import UIKit
 
 struct Fonts {
     static let body = UIFont(name: "SFProText-Regular", size: 17)
-    
     static let headline = UIFont(name: "SFProText-Semibold", size: 17)
     static let subheadline = UIFont(name: "SFProText-Regular", size: 15)
-    
+    static let callout = UIFont(name: "SFProText-Regular", size: 16)
+    static let footnote = UIFont(name: "SFProText-Regular", size: 13)
+    static let caption1 = UIFont(name: "SFProText-Semibold", size: 12)
     static let largeTitle = UIFont(name: "SFProText-Bold", size: 34)
+    static let title1 = UIFont(name: "SFProText-Bold", size: 28)
     static let title2 = UIFont(name: "SFProText-Bold", size: 24)
-    
     static let timer = UIFont(name: "SFProText-Bold", size: 42)
 }
