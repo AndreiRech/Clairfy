922d63f1d23dbf8a33c0d6ab64ccedbcb6b0d85b
Bernardo Garcia Fensterseifer <bernardofens@10-41-52-223.0200.epoa.br.iosda.org>
2025-06-20 17:52:40 -0300

    analysisFixAndAudioComponentFix

--- Clairfy/Components/AudioComponent.swift
+++ Clairfy/Components/AudioComponent.swift
@@ -22,6 +22,14 @@ class AudioComponent: UIView {
         view.layer.masksToBounds = true
         return view
     }()
+    
+    private lazy var soundWaveImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = .soundWave
+        return imageView
+    }()
 
     private lazy var audioTitleLabel: UILabel = {
         let label = UILabel()
@@ -38,9 +46,9 @@ class AudioComponent: UIView {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "10/06/2024 - 21:00"
+        label.textColor = .secondaryLabel
         label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         label.textAlignment = .left
-        label.textColor = UIColor(named: "Label-Secondary")
         label.numberOfLines = 1
         return label
     }()
@@ -49,92 +57,23 @@ class AudioComponent: UIView {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "00:48:14"
+        label.textColor = .secondaryLabel
         label.font = UIFont.monospacedDigitSystemFont(ofSize: 13, weight: .medium)
         label.textAlignment = .left
-        label.textColor = UIColor(named: "Label-Primary")
         label.numberOfLines = 1
         return label
     }()
 
     // MARK: - Buttons
-    private lazy var playImageView: UIImageView = {
-        let image = UIImageView()
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.image = UIImage(systemName: "play.fill")
-        image.contentMode = .scaleAspectFit
-        image.tintColor = .white
-        return image
-    }()
-
-    lazy var playButton: UIButton = {
+    private lazy var playButton: UIButton = {
         let button = UIButton(type: .system)
+        button.backgroundColor = .clairBlue
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .tertiarySystemBackground  // Corrigido: aplica direto no botão
+        button.layer.cornerRadius = 19
         return button
-    }()
-
-     lazy var playButtonView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .systemBlue
-        view.layer.cornerRadius = 25
-        view.layer.masksToBounds = true
-        return view
-    }()
-
-    private lazy var trashImageView: UIImageView = {
-        let image = UIImageView()
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.image = UIImage(systemName: "trash.fill")
-        image.contentMode = .scaleAspectFit
-        image.tintColor = .white
-        return image
-    }()
-
-    lazy var trashButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    private lazy var trashButtonView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .systemRed
-        view.layer.cornerRadius = 17
-        view.layer.masksToBounds = true
-        return view
-    }()
-
-    private lazy var shareImageView: UIImageView = {
-        let image = UIImageView()
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.image = UIImage(systemName: "square.and.arrow.up.fill")
-        image.contentMode = .scaleAspectFit
-        image.tintColor = .white
-        return image
-    }()
-
-    lazy var shareButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    private lazy var shareButtonView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .systemGreen
-        view.layer.cornerRadius = 17
-        view.layer.masksToBounds = true
-        return view
-    }()
-
-    private lazy var soundWaveImageView: UIImageView = {
-        let image = UIImageView()
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.image = UIImage(named: "soundWave")
-        image.contentMode = .scaleAspectFit
-        return image
     }()
 
     // MARK: - Stack Views
@@ -148,23 +87,8 @@ class AudioComponent: UIView {
         return stack
     }()
 
-    private lazy var buttonsStackView: UIStackView = {
-        let stack = UIStackView()
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.axis = .horizontal
-        stack.spacing = 12
-        stack.alignment = .center
-        stack.distribution = .fill
-        let spacer = UIView()
-        spacer.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        stack.addArrangedSubview(spacer)
-        stack.addArrangedSubview(trashButtonView)
-        stack.addArrangedSubview(shareButtonView)
-        return stack
-    }()
-
     private lazy var topStackView: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [playButtonView, audioInfoStackView, audioDurationLabel])
+        let stack = UIStackView(arrangedSubviews: [playButton, audioInfoStackView, audioDurationLabel])
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .horizontal
         stack.spacing = 16
@@ -174,7 +98,7 @@ class AudioComponent: UIView {
     }()
 
     private lazy var mainStackView: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [topStackView, soundWaveImageView, buttonsStackView])
+        let stack = UIStackView(arrangedSubviews: [topStackView, soundWaveImageView])
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
         stack.spacing = 16
@@ -213,6 +137,11 @@ class AudioComponent: UIView {
         get { audioDurationLabel.textColor }
         set { audioDurationLabel.textColor = newValue }
     }
+    
+    var playbutton: UIButton {
+        get { playButton }
+        set { playButton = newValue }
+    }
 
     var audioPath: String? {
         didSet {
@@ -221,57 +150,9 @@ class AudioComponent: UIView {
         }
     }
     
-    var playButtonColor: UIColor? {
-        get { playButtonView.backgroundColor }
-        set { playButtonView.backgroundColor = newValue }
-    }
-
-    var trashButtonColor: UIColor? {
-        get { trashButtonView.backgroundColor }
-        set { trashButtonView.backgroundColor = newValue }
-    }
-
-    var shareButtonColor: UIColor? {
-        get { shareButtonView.backgroundColor }
-        set { shareButtonView.backgroundColor = newValue }
-    }
-
-    var playButtonIconColor: UIColor? {
-        get { playImageView.tintColor }
-        set { playImageView.tintColor = newValue }
-    }
-
-    var trashButtonIconColor: UIColor? {
-        get { trashImageView.tintColor }
-        set { trashImageView.tintColor = newValue }
-    }
-
-    var shareButtonIconColor: UIColor? {
-        get { shareImageView.tintColor }
-        set { shareImageView.tintColor = newValue }
-    }
-    
     var playButtonState: PlayButtonState = .play {
-                didSet {
-                    updatePlayButtonIcon()
-                }
-            }
-
-    var playButtonIconWeight: UIImage.SymbolWeight = .medium {
         didSet {
-            updateIconWeight(imageView: playImageView, systemName: "play.fill", weight: playButtonIconWeight)
-        }
-    }
-
-    var trashButtonIconWeight: UIImage.SymbolWeight = .medium {
-        didSet {
-            updateIconWeight(imageView: trashImageView, systemName: "trash.fill", weight: trashButtonIconWeight)
-        }
-    }
-
-    var shareButtonIconWeight: UIImage.SymbolWeight = .medium {
-        didSet {
-            updateIconWeight(imageView: shareImageView, systemName: "square.and.arrow.up.fill", weight: shareButtonIconWeight)
+            updatePlayButtonIcon()
         }
     }
 
@@ -288,25 +169,24 @@ class AudioComponent: UIView {
 
     // MARK: - Button Animations
     private func setupButtonAnimations() {
-        let buttons = [playButton, trashButton, shareButton]
-        buttons.forEach { button in
-            button.addTarget(self, action: #selector(buttonTouchDown(_:)), for: [.touchDown, .touchDragEnter])
-            button.addTarget(self, action: #selector(buttonTouchUp(_:)), for: [.touchUpInside, .touchUpOutside, .touchDragExit, .touchCancel])
-        }
+        let button = playButton
+        button.addTarget(self, action: #selector(buttonTouchDown(_:)), for: [.touchDown, .touchDragEnter])
+        button.addTarget(self, action: #selector(buttonTouchUp(_:)), for: [.touchUpInside, .touchUpOutside, .touchDragExit, .touchCancel])
     }
     
     private func updatePlayButtonIcon() {
-            let systemName: String
-            switch playButtonState {
+        let systemName: String
+        switch playButtonState {
             case .play:
                 systemName = "play.fill"
             case .pause:
                 systemName = "pause.fill"
-            }
-            
-            let config = UIImage.SymbolConfiguration(weight: playButtonIconWeight)
-            playImageView.image = UIImage(systemName: systemName, withConfiguration: config)
         }
+        
+        // Usando peso bold como padrão
+        let config = UIImage.SymbolConfiguration(weight: .bold)
+        playButton.setImage(UIImage(systemName: systemName, withConfiguration: config), for: .normal)
+    }
 
 
     @objc private func buttonTouchDown(_ sender: UIButton) {
@@ -360,15 +240,6 @@ extension AudioComponent: ViewCodeProtocol {
     func addSubViews() {
         addSubview(backgroundView)
         backgroundView.addSubview(mainStackView)
-
-        playButtonView.addSubview(playImageView)
-        playButtonView.addSubview(playButton)
-
-        trashButtonView.addSubview(trashImageView)
-        trashButtonView.addSubview(trashButton)
-
-        shareButtonView.addSubview(shareImageView)
-        shareButtonView.addSubview(shareButton)
     }
 
     func setupConstraints() {
@@ -384,50 +255,9 @@ extension AudioComponent: ViewCodeProtocol {
             mainStackView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: padding),
             mainStackView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -padding),
             mainStackView.bottomAnchor.constraint(equalTo: backgroundView.bottomAnchor, constant: -padding),
-
-            // Play button constraints
-            playButtonView.widthAnchor.constraint(equalToConstant: 50),
-            playButtonView.heightAnchor.constraint(equalToConstant: 50),
-            playImageView.centerXAnchor.constraint(equalTo: playButtonView.centerXAnchor),
-            playImageView.centerYAnchor.constraint(equalTo: playButtonView.centerYAnchor),
-            playImageView.widthAnchor.constraint(equalToConstant: 20),
-            playImageView.heightAnchor.constraint(equalToConstant: 20),
-            playButton.topAnchor.constraint(equalTo: playButtonView.topAnchor),
-            playButton.leadingAnchor.constraint(equalTo: playButtonView.leadingAnchor),
-            playButton.trailingAnchor.constraint(equalTo: playButtonView.trailingAnchor),
-            playButton.bottomAnchor.constraint(equalTo: playButtonView.bottomAnchor),
-
-            // Trash button constraints
-            trashButtonView.widthAnchor.constraint(equalToConstant: 34),
-            trashButtonView.heightAnchor.constraint(equalToConstant: 34),
-            trashImageView.centerXAnchor.constraint(equalTo: trashButtonView.centerXAnchor),
-            trashImageView.centerYAnchor.constraint(equalTo: trashButtonView.centerYAnchor),
-            trashImageView.widthAnchor.constraint(equalToConstant: 20),
-            trashImageView.heightAnchor.constraint(equalToConstant: 20),
-            trashButton.topAnchor.constraint(equalTo: trashButtonView.topAnchor),
-            trashButton.leadingAnchor.constraint(equalTo: trashButtonView.leadingAnchor),
-            trashButton.trailingAnchor.constraint(equalTo: trashButtonView.trailingAnchor),
-            trashButton.bottomAnchor.constraint(equalTo: trashButtonView.bottomAnchor),
-
-            // Share button constraints
-            shareButtonView.widthAnchor.constraint(equalToConstant: 34),
-            shareButtonView.heightAnchor.constraint(equalToConstant: 34),
-            shareImageView.centerXAnchor.constraint(equalTo: shareButtonView.centerXAnchor),
-            shareImageView.centerYAnchor.constraint(equalTo: shareButtonView.centerYAnchor),
-            shareImageView.widthAnchor.constraint(equalToConstant: 20),
-            shareImageView.heightAnchor.constraint(equalToConstant: 20),
-            shareButton.topAnchor.constraint(equalTo: shareButtonView.topAnchor),
-            shareButton.leadingAnchor.constraint(equalTo: shareButtonView.leadingAnchor),
-            shareButton.trailingAnchor.constraint(equalTo: shareButtonView.trailingAnchor),
-            shareButton.bottomAnchor.constraint(equalTo: shareButtonView.bottomAnchor),
-
-            // Tamanho dos ícones dos botões
-            playImageView.widthAnchor.constraint(equalTo: playButtonView.widthAnchor, multiplier: 0.5),
-            playImageView.heightAnchor.constraint(equalTo: playButtonView.heightAnchor, multiplier: 0.5),
-            trashImageView.widthAnchor.constraint(equalTo: trashButtonView.widthAnchor, multiplier: 0.5),
-            trashImageView.heightAnchor.constraint(equalTo: trashButtonView.heightAnchor, multiplier: 0.5),
-            shareImageView.widthAnchor.constraint(equalTo: shareButtonView.widthAnchor, multiplier: 0.5),
-            shareImageView.heightAnchor.constraint(equalTo: shareButtonView.heightAnchor, multiplier: 0.5),
+            
+            playButton.heightAnchor.constraint(equalToConstant: 38),
+            playButton.widthAnchor.constraint(equalToConstant: 38),
 
             // Sound wave image
             soundWaveImageView.heightAnchor.constraint(equalToConstant: 40),
--- Clairfy/Features/Analysis/DoctorAnalysisView.swift
+++ Clairfy/Features/Analysis/DoctorAnalysisView.swift
@@ -10,29 +10,9 @@ import UIKit
 
 class DoctorAnalysisView: UIView {
     
-    // MARK: - Properties
-    var consultation: ConsultationModel? {
-        didSet {
-            updateUI()
-            analysisGenerated = consultation?.transcription != nil
-
-        }
-    }
-    
-    private var isLoading: Bool = false {
-        didSet {
-            updateLoadingState()
-        }
-    }
-    
-    private var analysisGenerated: Bool = false {
-        didSet {
-            updateContentVisibility()
-        }
-    }
-    
     // MARK: - UI Components
-    private lazy var clinicalSummary: TextComponent = {
+    // será arrumado esse componente depois...
+    internal lazy var clinicalSummary: TextComponent = {
         let textComponent = TextComponent()
         textComponent.translatesAutoresizingMaskIntoConstraints = false
         textComponent.title = "Resumo Clínico"
@@ -54,7 +34,8 @@ class DoctorAnalysisView: UIView {
         return textComponent
     }()
     
-    private lazy var actionPoints: TextComponent = {
+    // será arrumado esse componente depois...
+    internal lazy var actionPoints: TextComponent = {
         let textComponent = TextComponent()
         textComponent.translatesAutoresizingMaskIntoConstraints = false
         textComponent.title = "Pontos de Ação"
@@ -72,33 +53,8 @@ class DoctorAnalysisView: UIView {
         
         return textComponent
     }()
-    
-    private lazy var generateAnalysisButton: GlassButton = {
-        let button = GlassButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        
-        button.buttonText = "Gerar Análise"
-        button.textColor = .clairBlue
-        button.fontSize = 22
-        button.fontWeight = .semibold
-        button.blurOpacity = 0.35
-        button.glassBorderWidth = 1.0
-        button.cornerRadius = 24
-        button.heightAnchor.constraint(equalToConstant: 56).isActive = true
-        
-        button.addTarget(self, action: #selector(gerarAnalise), for: .touchUpInside)
-        
-        return button
-    }()
-    
-    private lazy var loadingView: LoaderView = {
-        let view = LoaderView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.isHidden = true
-        return view
-    }()
-    
-    private lazy var contentStackView: UIStackView = {
+
+    internal lazy var contentStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [clinicalSummary, actionPoints])
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
@@ -111,8 +67,6 @@ class DoctorAnalysisView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
-        
-        updateUI()
     }
     
     required init?(coder: NSCoder) {
@@ -122,183 +76,24 @@ class DoctorAnalysisView: UIView {
     // MARK: - Private Methods
     private func setupViews() {
         addSubview(contentStackView)
-        addSubview(generateAnalysisButton)
-        addSubview(loadingView)
         
         NSLayoutConstraint.activate([
             contentStackView.topAnchor.constraint(equalTo: topAnchor),
             contentStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             contentStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            contentStackView.bottomAnchor.constraint(lessThanOrEqualTo: generateAnalysisButton.topAnchor, constant: -24),
-            
-            generateAnalysisButton.leadingAnchor.constraint(equalTo: leadingAnchor),
-            generateAnalysisButton.trailingAnchor.constraint(equalTo: trailingAnchor),
-            generateAnalysisButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -24),
-            
-            loadingView.topAnchor.constraint(equalTo: topAnchor),
-            loadingView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            loadingView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            loadingView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            contentStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
-    
-    private func updateUI() {
-        clinicalSummary.text = consultation?.transcription?.summary ?? "Nenhum resumo disponível"
-        actionPoints.text = consultation?.transcription?.actionPoints.joined(separator: "\n\n") ?? "Nenhum ponto de ação disponível"
-    }
-
-    private func updateLoadingState() {
-        loadingView.isHidden = !isLoading
-        generateAnalysisButton.isEnabled = !isLoading
-        generateAnalysisButton.alpha = isLoading ? 0.7 : 1.0
-    }
-
-    // arrumar - duracao tem q ser de acordo com o tempo de resposta da api
-    private func updateContentVisibility() {
-        UIView.animate(withDuration: 0.3) {
-            self.contentStackView.isHidden = !self.analysisGenerated
-            self.contentStackView.alpha = self.analysisGenerated ? 1.0 : 0.0
-        }
-    }
-    
+     
     // MARK: - Button Actions
-    @objc private func editButtonTapped() {
+    @objc internal func editButtonTapped() {
         print("Botão Editar (Médico) pressionado")
     }
     
-    @objc private func copyButtonTapped() {
+    @objc internal func copyButtonTapped() {
         print("Botão Copiar (Médico) pressionado")
     }
     
     var tempoDeRespostaAPI: Bool = false
     
-    @objc func gerarAnalise() {
-        guard let audioPath = consultation?.audio?.audioPath else {
-            print("❌ Caminho do áudio não encontrado. \(consultation?.audio?.audioPath ?? "Nenhum")")
-            return
-        }
-
-        isLoading = true
-
-        let fileManager = FileManager.default
-        let documentsDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        let originalURL = documentsDir.appendingPathComponent(audioPath)
-        let destinationURL = documentsDir.appendingPathComponent("audioTranscricao.m4a")
-
-        do {
-            if fileManager.fileExists(atPath: destinationURL.path) {
-                try fileManager.removeItem(at: destinationURL)
-            }
-            try fileManager.copyItem(at: originalURL, to: destinationURL)
-            print("📥 Áudio copiado para: \(destinationURL.path)")
-        } catch {
-            print("❌ Erro ao copiar o áudio:", error.localizedDescription)
-            isLoading = false
-            return
-        }
-
-        let api = APIchatGPT()
-        print("🎙️ Iniciando transcrição...")
-
-        api.transcreverAudio(audioFileURL: destinationURL) { [weak self] transcricao in
-            guard let self = self else { return }
-
-            DispatchQueue.main.async {
-                guard let transcricao = transcricao, !transcricao.isEmpty else {
-                    print("❌ Transcrição vazia ou nula.")
-                    self.isLoading = false
-                    return
-                }
-
-                print("📝 Transcrição recebida:")
-                print(transcricao)
-
-                print("💬 Enviando para resumo...")
-
-                api.resumirTexto(transcricao, type: "doctor") { resultDoctor in
-                    DispatchQueue.main.async {
-                        guard let resultDoctor = resultDoctor, let jsonDoctorData = resultDoctor.data(using: .utf8) else {
-                            print("❌ Erro ao receber ou converter o resumo do doctor.")
-                            self.isLoading = false
-                            return
-                        }
-
-                        var summary: String?
-                        var keyWords: [String] = []
-
-                        do {
-                            let decoder = JSONDecoder()
-                            let apiResponseDoctor = try decoder.decode(DoctorResponse.self, from: jsonDoctorData)
-                            summary = apiResponseDoctor.summary
-                            keyWords = apiResponseDoctor.keyWords
-                            print("✅ Doctor Summary: \(summary ?? "Nenhum")")
-                            print("✅ Doctor Keywords: \(keyWords)")
-                        } catch {
-                            print("❌ Erro ao decodificar JSON do doctor: \(error.localizedDescription)")
-                            self.isLoading = false
-                            return
-                        }
-
-                        api.resumirTexto(transcricao, type: "patient") { resultPatient in
-                            DispatchQueue.main.async {
-                                guard let resultPatient = resultPatient, let jsonPatientData = resultPatient.data(using: .utf8) else {
-                                    print("❌ Erro ao receber ou converter o resumo do patient.")
-                                    self.isLoading = false
-                                    return
-                                }
-
-                                var didctarized: String?
-                                var actionPoints: [String] = []
-
-                                do {
-                                    let decoder = JSONDecoder()
-                                    let apiResponsePatient = try decoder.decode(PatientResponse.self, from: jsonPatientData)
-                                    didctarized = apiResponsePatient.didctarized
-                                    actionPoints = apiResponsePatient.actionPoints
-                                    print("✅ Patient Didctarized: \(didctarized ?? "Nenhum")")
-                                    print("✅ Patient ActionPoints: \(actionPoints)")
-                                } catch {
-                                    print("❌ Erro ao decodificar JSON do patient: \(error.localizedDescription)")
-                                    self.isLoading = false
-                                    return
-                                }
-
-                                // Se chegou aqui, tudo foi bem com as duas APIs.
-                                guard let summary = summary, let didctarized = didctarized else {
-                                    print("❌ Dados incompletos após os dois resumos.")
-                                    self.isLoading = false
-                                    return
-                                }
-
-                                let transcricaoModel = TranscriptionModel(
-                                    id: UUID(),
-                                    transcription: transcricao,
-                                    summary: summary,
-                                    didctarized: didctarized,
-                                    keyWords: keyWords,
-                                    actionPoints: actionPoints
-                                )
-
-                                Persistence.shared.createTranscription(transcricaoModel)
-
-                                guard let consultation = self.consultation else {
-                                    print("❌ Consulta não encontrada.")
-                                    self.isLoading = false
-                                    return
-                                }
-
-                                _ = Persistence.shared.updateConsultation(consultation, transcription: transcricaoModel, audio: nil)
-                                
-                                self.consultation = Persistence.shared.getConsultation(by: consultation.id)
-                                
-                                self.isLoading = false
-                                self.analysisGenerated = true
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
 }
