96ef7acb59501bb4eea690d960540bb5c4c0588b
Bernardo Garcia Fensterseifer <bernardofens@Bernardos-MacBook-Pro.local>
2025-07-03 17:15:48 -0300

    feat: ajustes finais no MicrophoneManager e tela de permissão

--- Clairfy/Utils/Managers/MicrophoneManager.swift
+++ Clairfy/Utils/Managers/MicrophoneManager.swift
@@ -1,22 +1,63 @@
+//import AVFAudio
+//import UIKit
+//
+//final class MicrophonePermissionManager {
+//    static func requestPermission(completion: @escaping (Bool) -> Void) {
+//        AVAudioApplication.requestRecordPermission { granted in
+//            DispatchQueue.main.async {
+//                completion(granted)
+//            }
+//        }
+//    }
+//    
+//    static func showPermissionAlert(on viewController: UIViewController) {
+//        let alert = UIAlertController(
+//            title: "Permissão Necessária",
+//            message: "Este app precisa de acesso ao microfone para gravar áudio. Vá em Ajustes > Privacidade > Microfone e ative o acesso.",
+//            preferredStyle: .alert
+//        )
+//        
+//        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+//        alert.addAction(UIAlertAction(title: "Abrir Ajustes", style: .default) { _ in
+//            if let settingsURL = URL(string: UIApplication.openSettingsURLString),
+//               UIApplication.shared.canOpenURL(settingsURL) {
+//                UIApplication.shared.open(settingsURL)
+//            }
+//        })
+//
+//        viewController.present(alert, animated: true, completion: nil)
+//    }
+//}
+
+/// agora suporta versões mais antigas do iOS!
+
 import AVFAudio
 import UIKit
 
 final class MicrophonePermissionManager {
     static func requestPermission(completion: @escaping (Bool) -> Void) {
-        AVAudioApplication.requestRecordPermission { granted in
-            DispatchQueue.main.async {
-                completion(granted)
+        if #available(iOS 17.0, *) {
+            AVAudioApplication.requestRecordPermission { granted in
+                DispatchQueue.main.async {
+                    completion(granted)
+                }
+            }
+        } else {
+            AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                DispatchQueue.main.async {
+                    completion(granted)
+                }
             }
         }
     }
-    
+
     static func showPermissionAlert(on viewController: UIViewController) {
         let alert = UIAlertController(
             title: "Permissão Necessária",
             message: "Este app precisa de acesso ao microfone para gravar áudio. Vá em Ajustes > Privacidade > Microfone e ative o acesso.",
             preferredStyle: .alert
         )
-        
+
         alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "Abrir Ajustes", style: .default) { _ in
             if let settingsURL = URL(string: UIApplication.openSettingsURLString),
